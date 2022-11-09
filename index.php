744ff1fb9590fd03a577a7de10e45aa9cfbefc81
portag <emiliodavid.zrgn@gmail.com>
2022-11-09 17:44:39 +0100

    actu

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php echo "hola mundo"; ?>
+<?php echo "para conectar con el repositorio <br>
+se hace un git init en la carpeta app <br>
+git remote add origin https://github.com/portag/juegosSCRUM.git <br>
+gir branch -m main, con eso estamos, luego va lo del git add .  commit y push"; ?>
