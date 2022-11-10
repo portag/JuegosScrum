6d30befb8ec33cd09be0fb0ea7541c57d98ccff7
guillehub <guillejp98@gmail.com>
2022-11-10 16:56:28 +0100

    1656

--- lib.php
+++ lib.php
@@ -1,23 +1,34 @@
 <?php
 
 
-  //Función para limpiar los input de los formularios
-  function filtrado($datos){
-    $datos = trim($datos); // Elimina espacios antes y después de los datos
-    $datos = stripslashes($datos); // Elimina backslashes \
-    $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
-    return $datos;
-  }
+//Función para limpiar los input de los formularios
+function filtrado($datos)
+{
+  $datos = trim($datos); // Elimina espacios antes y después de los datos
+  $datos = stripslashes($datos); // Elimina backslashes \
+  $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
+  return $datos;
+}
 
-
-
-
-
-
-
-
-
-
-
-  
-?>
+function pintarJuego()
+{
+  echo "<table class='table table-bordered text-center' style='font-size: 15px;' id='dataTable' width='100%' cellspacing='0'>";
+  //CABECERA                  
+  echo "<thead>";
+  echo "<tr>";
+   echo "<th>Titulo</th>";
+   echo "<th>Descripcion</th>";
+  echo "<th>Plataforma</th>";
+    echo  "<th>Género</th>";
+   echo  "</tr>";
+    echo "</thead>";
+    echo "<tfoot>";
+    echo" <tr>";
+   echo "<th>Titulo</th>";
+   echo "<th>Descripcion</th>";
+    echo "<th>Plataforma</th>";
+   echo "<th>Género</th>";
+    echo  "</tr>";
+   echo "</tfoot>";
+    echo  "<tbody>";
+}
