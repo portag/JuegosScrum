48841fe3b9a5d238a24a78f4355e6a9554c0ea58
emanueldelgadoU <emanueldelgado.u@gmail.com>
2022-11-13 18:42:30 +0100

    algunasCositas

--- lib.php
+++ lib.php
@@ -1,106 +1,81 @@
 <?php
-
+include_once('modelo.php');
 
 //Función para limpiar los input de los formularios
-function filtrado($datos)
-{
+function filtrado($datos){
     $datos = trim($datos); // Elimina espacios antes y después de los datos
     $datos = stripslashes($datos); // Elimina backslashes \
     $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
     return $datos;
 }
-?>
-<?php
+
 function pintarJuegos (){
-include('modelo.php');
-$juegos = selectJuegos();
+    
+    $juegos = selectJuegos();
 
-echo "<ul class='list-group'>";
-//Cabecera
-echo "<li class='list-group-item fw-bold'>";
-echo "<div class='row'>";
-echo "<div class='col'>ID</div>";
-echo "<div class='col'>Titulo</div>";
-echo "<div class='col'>Descripción</div>";
-echo "<div class='col'>Plataforma</div>";
-echo "<div class='col'>Genero</div>";
-echo "<div class='col'>Acciones</div>";
-echo "</div>";
-echo "</li>";
-//Contenido
-foreach ($juegos as $juego) {
-    echo "<li class='list-group-item'>";
-    echo "<div class='row'>";
+    echo "<table class='table'>";
+    //Cabecera
+    echo "<tr>";
+    echo "<th> ID </th>";
+    echo "<th> Titulo </th>";
+    echo "<th> Descripción </th>";
+    echo "<th> Plataforma </th>";
+    echo "<th> Genero </th>";
+    echo "<th> Acciones </th>";
+    echo "</tr>";
 
-    foreach ($juego as $key => $value) {
-            echo "<div class='col'>";
-            echo $value;
-            echo "</div>";
-        
-    }
-
+    //Contenido
+    foreach ($juegos as $juego) {
+    echo '<tr>';
+    echo '<td>'.$juego['id'].'</td>';
+    echo '<td>'.$juego['titulo'].'</td>';
+    echo '<td>'.$juego['descripcion'].'</td>';
+    echo '<td>'.$juego['plataforma'].'</td>';
+    echo '<td>'.$juego['genero'].'</td>';
+ 
     //Acciones
-    echo "<div class='col'>";
-    echo "<div class='col'>";
-    echo "<a href='controlador.php?accion=infoLocalizacion&id=" . $juego['id'] . "' class='btn btn-info btn-circle' style='--bs-btn-color: #ffffff; --bs-btn-hover-color: #fff;'> <i
+    echo "<td><a href='controlador.php?accion=infoLocalizacion&id=" . $juego['id'] . "' class='btn btn-info btn-circle' style='--bs-btn-color: #ffffff; --bs-btn-hover-color: #fff;'> <i
     class='fas fa-map'></i></a>";
-    echo "<a href='controlador.php?accion=borrar&id=" . $juego['id'] . "'  class='btn btn-danger btn-circle'><i class='fas fa-trash' ></i></a>";
-    echo "</div>";
-    echo "</div>";
+    echo "<a href='controlador.php?accion=borrar&id=" . $juego['id'] . "'  class='btn btn-danger btn-circle'><i class='fas fa-trash' ></i></a></td>";
 
-    echo "</div>";
-    echo "</li>";
-}
-echo "</ul>";
-
-
-}
-?>
-<?php
-function pintarLocalizacion (){
-include('modelo.php');
-$localizaciones = selectLocalizacion($idJuegos);
-
-echo "<ul class='list-group'>";
-//Cabecera
-echo "<li class='list-group-item fw-bold'>";
-echo "<div class='row'>";
-echo "<div class='col'>Nombre</div>";
-echo "<div class='col'>Descripción</div>";
-echo "<div class='col'>pInteres</div>";
-echo "<div class='col'>Importancia</div>";
-echo "<div class='col'>idJuego</div>";
-echo "<div class='col'>Acciones</div>";
-
-echo "</div>";
-echo "</li>";
-//Contenido
-foreach ($localizaciones as $localizacion) {
-    echo "<li class='list-group-item'>";
-    echo "<div class='row'>";
-
-    foreach ($localizacion as $key => $value) {
-        if ($key != "id") {
-            echo "<div class='col'>";
-            echo $value;
-            echo "</div>";
-        }
+    echo "</tr>";
+    
     }
+    echo "</table>";
 
-    //Acciones
-    echo "<div class='col'>";
-    echo "<div class='col'>";
-    echo "<a href='controlador.php?accion=borrarLocalizacion&id=" . $localizacion['nombre'] . "'  class='btn btn-danger btn-circle'><i class='fas fa-trash' ></i></a>";
-    echo "</div>";
-    echo "</div>";
-
-    echo "</div>";
-    echo "</li>";
 }
-echo "</ul>";
+
+function pintarLocalizacion ($idJuegos){
+
+        $localizaciones = selectLocalizacion($idJuegos);
+
+        echo "<table class='table'>";
+        //Cabecera
+        echo "<tr>";
+        echo "<th> Nombre </th>";
+        echo "<th> Descripción </th>";
+        echo "<th> pInteres </th>";
+        echo "<th> Importancia </th>";
+        echo "<th> idJuego </th>";
+        echo "<th> Acciones </th>";
+        echo "</tr>";
 
 
+    //Contenido
+    foreach ($localizaciones as $localizacion) {
+        echo "<tr>";
+        echo "<td>".$localizacion['nombre']."</td>";
+        echo "<td>".$localizacion['descripcion']."</td>";
+        echo "<td>".$localizacion['pInteres']."</td>";
+        echo "<td>".$localizacion['importancia']."</td>";
+        echo "<td>".$localizacion['idjuegos']."</td>";
+        echo "<td> <a href='controlador.php?accion=borrarLocalizacion&idLoca=".$localizacion['idLoca']."'class='btn btn-danger btn-circle'><i class='fas fa-trash' ></i></a></td>";
+        echo "</tr>";
+
+    }
+        
+    echo "</table>";
 }
 ?>
-</tbody>
-</table>
+
+
