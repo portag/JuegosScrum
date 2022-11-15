7878d62ca7d3c1ca81543ce83e086cc609b2611d
portag <emiliodavid.zrgn@gmail.com>
2022-11-15 19:56:14 +0100

    commit

--- modelo.php
+++ modelo.php
@@ -1,140 +1,153 @@
 <?php
 
-    //CONECCION A BASE DE DATOS HEROKU
-    // function conexionBD2() {
-    //     $dbh = null;
+//CONECCION A BASE DE DATOS HEROKU
+function conexionBD()
+{
+    $dbh = null;
 
-    //     try {
-    //         //mariadb --> nombre del contenedor donde tengamos mysql
-    //         //FALTA CONECTARTLO A HEROKU 
-    //         $dsn = "mysql:host=mysql;dbname=servidor";
-    //         $dbh = new PDO($dsn, "root", "toor");
-    //     } catch (PDOException $e){
-    //         echo $e->getMessage();
-    //     }
-
-    //     return $dbh;
-    // }
-
-
-    //CONECCION A BASE DE DATOS LOCAL
-    function conexionBD() {
-        $dbh = null;
-
-        try {
-            //mariadb --> nombre del contenedor donde tengamos mysql
-
-            $dsn = "mysql:host=mariadb2;dbname=scrum";
-            $dbh = new PDO($dsn, "root", "toor");
-        } catch (PDOException $e){
-            echo $e->getMessage();
-        }
-
-        return $dbh;
+    try {
+        //         //mariadb --> nombre del contenedor donde tengamos mysql
+        //         //FALTA CONECTARTLO A HEROKU 
+        $dsn = "mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_b15a104a533637d";
+        $dbh = new PDO($dsn, "b694b3e375ac30", "cde4ad6f");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
     }
 
+    return $dbh;
+}
 
-    //FUNCION INSERTAR JUEGO EN LA BASE DE DATOS
-    function insertarJuego($titulo, $descripcion, $plataforma, $genero) {
-        
-        $conexion = conexionBD();
+/**
+ * 
+ * 
+ * function conexionBD() {
+ *       $dbh = null;
+*
+ *       try {
+  *          //mariadb --> nombre del contenedor donde tengamos mysql
+*
+ *           $dsn = "mysql:host=mariadb2;dbname=scrum";
+  *          $dbh = new PDO($dsn, "root", "toor");
+   *     } catch (PDOException $e){
+    *        echo $e->getMessage();
+     *   }
+*
+ *       return $dbh;
+  *  }
+ * 
+ */
 
-        try {
-            $stmt = $conexion->prepare("INSERT INTO juegos (titulo, descripcion, plataforma, genero) VALUES (?, ?, ?, ?)" );
-    
-            $stmt->bindValue(1, $titulo);
-            $stmt->bindValue(2, $descripcion);
-            $stmt->bindValue(3, $plataforma);
-            $stmt->bindValue(4, $genero);
-            $stmt->execute();
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }
+
+
+
+//FUNCION INSERTAR JUEGO EN LA BASE DE DATOS
+function insertarJuego($titulo, $descripcion, $plataforma, $genero)
+{
+
+    $conexion = conexionBD();
+
+    try {
+        $stmt = $conexion->prepare("INSERT INTO juegos (titulo, descripcion, plataforma, genero) VALUES (?, ?, ?, ?)");
+
+        $stmt->bindValue(1, $titulo);
+        $stmt->bindValue(2, $descripcion);
+        $stmt->bindValue(3, $plataforma);
+        $stmt->bindValue(4, $genero);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
     }
+}
 
-    //FUNCION INSERTAR LOCALIZACION EN LA BASE DE DATOS
-    function insertarLocalizacion($nombre, $descripcion, $pInteres, $importancia, $idJuegos ){
-        
-        $conexion = conexionBD();
-    
-        try {
-            $stmt = $conexion->prepare("INSERT INTO localizaciones (nombre, descripcion, pInteres, importancia, idjuegos) VALUES (?, ?, ?, ?, ?)");
-    
-            $stmt->bindValue(1, $nombre);
-            $stmt->bindValue(2, $descripcion);
-            $stmt->bindValue(3, $pInteres);
-            $stmt->bindValue(4, $importancia);
-            $stmt->bindValue(5, $idJuegos);
-            //$_GET['$idJuego']
+//FUNCION INSERTAR LOCALIZACION EN LA BASE DE DATOS
+function insertarLocalizacion($nombre, $descripcion, $pInteres, $importancia, $idJuegos)
+{
 
-            $stmt->execute();
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }
+    $conexion = conexionBD();
+
+    try {
+        $stmt = $conexion->prepare("INSERT INTO localizaciones (nombre, descripcion, pInteres, importancia, idjuegos) VALUES (?, ?, ?, ?, ?)");
+
+        $stmt->bindValue(1, $nombre);
+        $stmt->bindValue(2, $descripcion);
+        $stmt->bindValue(3, $pInteres);
+        $stmt->bindValue(4, $importancia);
+        $stmt->bindValue(5, $idJuegos);
+        //$_GET['$idJuego']
+
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
     }
+}
 
 
-    //FUNCION BORRAR JUEGO DE LA BASE DE DATOS
-    function borrarJuego($idJuego) {
-        $conexion = conexionBD();
+//FUNCION BORRAR JUEGO DE LA BASE DE DATOS
+function borrarJuego($idJuego)
+{
+    $conexion = conexionBD();
 
-        try {
-            $stmt = $conexion->prepare("DELETE FROM juegos WHERE id = ?");
-            $stmt->bindValue(1, $idJuego);
-            $stmt->execute();
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }
-        $conexion = null; //Cerrar la conexión
+    try {
+        $stmt = $conexion->prepare("DELETE FROM juegos WHERE id = ?");
+        $stmt->bindValue(1, $idJuego);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
     }
-    //MOSTRAR TABLA juegos DE LA BASE DE DATOS
-    function selectJuegos() {
+    $conexion = null; //Cerrar la conexión
+}
+//MOSTRAR TABLA juegos DE LA BASE DE DATOS
+function selectJuegos()
+{
 
-        $conexion = conexionBD();
-        $tareas = null;
+    $conexion = conexionBD();
+    $tareas = null;
 
-        try {
-            $stmt = $conexion->prepare("SELECT * FROM juegos");
-            $stmt->execute();
-            $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }   
-        $conexion = null; //Cerrar la conexión
-
-        return $tareas;
+    try {
+        $stmt = $conexion->prepare("SELECT * FROM juegos");
+        $stmt->execute();
+        $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
     }
+    $conexion = null; //Cerrar la conexión
 
-    function borrarLocalizacion($idLoca) {
+    return $tareas;
+}
 
-        $conexion = conexionBD();
+function borrarLocalizacion($idLoca)
+{
 
-        try {
-            $stmt = $conexion->prepare("DELETE FROM localizaciones WHERE idLoca = ?");
-            $stmt->bindValue(1, $idLoca);
-            $stmt->execute();
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }
-        $conexion = null; //Cerrar la conexión
+    $conexion = conexionBD();
+
+    try {
+        $stmt = $conexion->prepare("DELETE FROM localizaciones WHERE idLoca = ?");
+        $stmt->bindValue(1, $idLoca);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
     }
+    $conexion = null; //Cerrar la conexión
+}
 
-    function borrarLocalizacion2($ids) {
-        $conexion = conexionBD();
+function borrarLocalizacion2($ids)
+{
+    $conexion = conexionBD();
 
-        try {
-            $stmt = $conexion->prepare("DELETE FROM localizaciones WHERE idLoca = ?");
-            $stmt->bindValue(1, $ids);
-            $stmt->execute();
-        } catch (PDOException $ex) {
-            echo $ex->getMessage();
-        }
-        $conexion = null; //Cerrar la conexión
+    try {
+        $stmt = $conexion->prepare("DELETE FROM localizaciones WHERE idLoca = ?");
+        $stmt->bindValue(1, $ids);
+        $stmt->execute();
+    } catch (PDOException $ex) {
+        echo $ex->getMessage();
     }
+    $conexion = null; //Cerrar la conexión
+}
 
 
 //MOSTRAR TABLA juegos DE LA BASE DE DATOS
-function selectLocalizacion($idJuegos) {
+function selectLocalizacion($idJuegos)
+{
 
     $conexion = conexionBD();
     $tareas = null;
@@ -146,20 +159,8 @@ function selectLocalizacion($idJuegos) {
         $tareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $ex) {
         echo $ex->getMessage();
-    }   
+    }
     $conexion = null; //Cerrar la conexión
 
     return $tareas;
 }
-
-
-
-
-
-
-
-
-
-
-  
-?>
